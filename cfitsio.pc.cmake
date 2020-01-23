41141309063b6b38c0411216093c637b59f6a0d3
Davide Punzo <punzodavide@hotmail.it>
2020-01-23 13:22:26 +0100

    COMP: update to 3.470

--- cfitsio.pc.cmake
+++ cfitsio.pc.cmake
@@ -1,12 +1,12 @@
-prefix=@prefix@
-exec_prefix=@exec_prefix@
-libdir=@libdir@
-includedir=@includedir@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=@CMAKE_INSTALL_PREFIX@
+libdir=@PKG_CONFIG_LIBDIR@
+includedir=@INCLUDE_INSTALL_DIR@
 
 Name: cfitsio
 Description: FITS File Subroutine Library
 URL: https://heasarc.gsfc.nasa.gov/fitsio/
 Version: @CFITSIO_MAJOR@.@CFITSIO_MINOR@
-Libs: -L${libdir} -lcfitsio @LIBS@
+Libs: -L${libdir} @PKG_CONFIG_LIBS@
 Libs.private: -lm
 Cflags: -I${includedir}
